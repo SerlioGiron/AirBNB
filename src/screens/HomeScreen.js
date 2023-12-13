e0d1b542a63ab41eeadef439fd118ad2113538f7
Daniel <danielisaacjuarez@gmail.com>
2023-12-13 14:18:01 -0600

    m

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {View, StyleSheet, ScrollView} from 'react-native';
 import {colors} from '.scr/components/constants/theme';
-import MainHeader from '.scr/components/MainHeader';
+import MainHeader from 'src/components/MainHeader.js';
 import ScreenHeader from '.scr/components/ScreenHeader';
 import TopPlacesCarousel from '.scr/components/TopPlacesCarousel';
 import {PLACES, TOP_PLACES} from '.scr/data';
